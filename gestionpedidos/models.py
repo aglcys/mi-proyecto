db74331a5867998b697442dbda8b70cbebd3d0a8
aglcys <angelicayanez30@gmail.com>
2022-10-22 21:12:09 -0300

    primer commit con modelos, migracion y 3 articulos en la base de datos de implementos

---
+++ gestionpedidos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Empleado(models.Model):
+    nombre=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField()
+    tlfno=models.CharField(max_length=20)
+
+class Implementos(models.Model):
+    nombre=models.CharField(max_length=30)
+    categoria=models.CharField(max_length=30)
+    cantidad=models.CharField(max_length=30)
+
+class Pedidos(models.Model):
+    numero=models.IntegerField()
+    fecha=models.DateField()
+    entregado=models.BooleanField()
